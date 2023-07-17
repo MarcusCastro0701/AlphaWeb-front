b54df4433fcea772ca1a7e55fe45a30a61a5836d
MarcusCastro0701 <marcus.castro0701@gmail.com>
2023-07-17 15:27:13 -0300

    start application

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useContext, useEffect } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+
+function App() {
+  return <Main />;
+}
+
+export default App;
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export default function Main() {
+  return (
+    <Container>
+      <p>OLÁ ALVINHO</p>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+background-color: lightblue;
+height: 200px;
+width: 200px;
+`;
