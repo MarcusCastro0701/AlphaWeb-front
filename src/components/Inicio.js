7523c4f98ca0f6cf66f7353107c544fcb6292dd3
MarcusCastro0701 <marcus.castro0701@gmail.com>
2023-07-24 14:44:31 -0300

    add: photo carousel

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,22 +1,27 @@
 import styled from "styled-components";
 import React, { useState } from 'react';
-import fotohome from "../assets/images/fotohome.jpg"
+import {BsArrowDownShort} from "react-icons/bs"
+import scroll from "../hooks/Scroll";
+
 
 export default function Inicio(){
 
     return(
         <Container>
             <SubContainerLeft>
-                <p>
-                    AlphaWeb
-                </p>
                 <h1>
-                    Desenvolvendo e aplicando com o melhor método.
+                    Desenvolvendo e aplicando 
                 </h1>
+                <p>
+                    com o melhor método.
+                </p>
                 <h2>
-                    Transforme sua presença online conosco: desenvolvimento web e marketing digital que impulsionam negócio.
+                    Transforme sua presença online conosco: desenvolvimento web e marketing digital que impulsionam o seu negócio.
                 </h2>
-
+                <div onClick={() => scroll(710)}>
+                    CONHEÇA A ALPHAWEB
+                </div>
+                <Icone onClick={() => scroll(710)}/>
             </SubContainerLeft>
 
             <SubContainerRight>
@@ -27,58 +32,76 @@ export default function Inicio(){
 }
 
 const Container = styled.div `
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: row;
-width: 90%;
-height: 600px;
-background-color: none;
-margin-top: 60px;
-border-radius: 15px;
-p{
-    font-size: 60px;
-    color: white;
-}
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    width: 90%;
+    height: 700px;
+    background-color: none;
+    margin-top: 60px;
+    border-radius: 15px;
+    p{
+        font-size: 20px;
+        color: #197fbe;
+    }
 `
 const SubContainerLeft = styled.div`
-width: 90%;
-height: 100%;
-border-radius: 15px 0 0 15px;
-padding-left: 10%;
-p{
-    font-size: 140px;
-    color: #3a76c9;
-}
-h1{
-    margin-top: 30px;
-    font-size: 20px;
-    color: white;
-}
-h2{
-    margin-top: 30px;
-    font-size: 40px;
-    color: white;
-}
+    width: 90%;
+    height: 100%;
+    border-radius: 15px 0 0 15px;
+    padding-left: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    p{
+        font-size: 40px;
+        color: #3a76c9;
+        margin-top: 8px;
+        font-weight: 700;
+    }
+    h1{
+        margin-top: 0px;
+        font-size: 40px;
+        color: white;
+        font-weight: 700;
+    }
+    h2{
+        margin: 60px 0 60px 0;
+        font-size: 23px;
+        color: white;
+        width: 800px;
+        text-align: center;
+    }
+    div{
+        width: 300px;
+        height: 50px;
+        color: white;
+        background-color: #3a76c9;
+        border: none;
+        border-radius: 8px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+    }
 `
 
-const ImgContainer = styled.div`
-display: flex;
-align-items: center;
-border-radius: 90px;
-img{
-    padding: 30px 0 0 90px;
-    width: 750px;
-    height: 400px;
-}
+const Icone = styled(BsArrowDownShort)`
+    height: 50px;
+    width: 50px;
+    color: white;
+    margin-top: 15px;
+    cursor: pointer;
 `
 
 const SubContainerRight = styled.div`
-width: 10%;
-height: 100%;
-border-radius: 0 15px 15px 0;
-p{
-    font-size: 20px;
-    color: white;
-}
+    width: 10%;
+    height: 100%;
+    border-radius: 0 15px 15px 0;
+    p{
+        font-size: 20px;
+        color: white;
+    }
 `
